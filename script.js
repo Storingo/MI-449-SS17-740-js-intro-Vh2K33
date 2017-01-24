80cb94071cdbd32b46ceac357781232b52fc322c
Dylan O'Dowd <Dylan O'Dowd>
2017-01-24 16:46:08 -0500

    created 3 buttons and made them each play a different sound

---
+++ script.js
@@ -0,0 +1,17 @@
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+
+var boom = document.getElementById('boom');
+var kick = document.getElementById('kick');
+var snare = document.getElementById('snare');
+
+button1.addEventListener('click', function () {
+  boom.play();
+});
+button2.addEventListener('click', function () {
+  kick.play();
+});
+button3.addEventListener('click', function () {
+  snare.play();
+});
